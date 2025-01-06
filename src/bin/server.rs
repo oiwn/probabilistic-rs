8bb8318e35dc6e19316ba4c6e8f34ef0b7846d44
oiwn <alex@imscraping.ninja>
2025-01-07 00:20:24 +0700

    readme

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -10,7 +10,6 @@ async fn main() {
     tracing_subscriber::fmt()
         .with_max_level(tracing::Level::INFO)
         .with_target(false)
-        .with_thread_ids(true)
         .with_file(true)
         .with_line_number(true)
         .init();
